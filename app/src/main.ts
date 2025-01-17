95fb3240c42ecd6bcd2ce13357d26e96852a8bec
yaroslavTsebro <yaroslav.cebro@gmail.com>
2025-01-17 16:01:06 +0200

    * minor changes

--- app/src/main.ts
+++ app/src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import * as cookieParser from 'cookie-parser';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
+import * as yaml from 'js-yaml';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,11 +14,15 @@ async function bootstrap() {
 
   app.use(cookieParser());
 
+  app.useGlobalPipes(new ValidationPipe({
+    transform: true,
+  }));
+
   const swaggerConfig = new DocumentBuilder()
     .setTitle('API Documentation')
     .setDescription('API endpoints and details')
     .setVersion('1.0')
-    .addBearerAuth({ 
+    .addBearerAuth({
       type: 'http',
       scheme: 'bearer',
       bearerFormat: 'JWT',
@@ -24,7 +30,14 @@ async function bootstrap() {
     .build();
 
   const document = SwaggerModule.createDocument(app, swaggerConfig);
+
   SwaggerModule.setup('api', app, document);
+
+  // TODO: for some reasons yaml endpoint doesn\'t work by default
+  app.getHttpAdapter().get('/api/yaml', (req, res) => {
+    res.setHeader('Content-Type', 'application/x-yaml');
+    res.send(yaml.dump(document));
+  });
 
   console.log(`Application is running on: http://localhost:${port}`);
   await app.listen(port);
