35debc63ff5e751986ffd33eb03e76c58033cc4e
yaroslavTsebro <yaroslav.cebro@gmail.com>
2025-01-17 16:01:20 +0200

    *  generated api

---
+++ web-ui/src/openapi/apis/index.ts
@@ -0,0 +1,7 @@
+// @ts-nocheck
+/* tslint:disable */
+/* eslint-disable */
+export * from './AuthApi';
+export * from './ProjectsApi';
+export * from './TasksApi';
+export * from './UsersApi';
---
+++ web-ui/src/openapi/models/index.ts
@@ -0,0 +1,12 @@
+// @ts-nocheck
+/* tslint:disable */
+/* eslint-disable */
+export * from './CreateTaskDto';
+export * from './CreateTaskListDto';
+export * from './EmailAuthPayload';
+export * from './InviteUserDto';
+export * from './RefreshPayload';
+export * from './TaskListControllerRemoveUserRequest';
+export * from './TokenResponse';
+export * from './UpdateProjectDto';
+export * from './UpdateTaskDto';
